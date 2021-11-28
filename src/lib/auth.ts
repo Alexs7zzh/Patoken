73e18f7ac54d63c139e2c3a765c453b6acb2e8dd
Alex Zhou <alex.zzh.s7@gmail.com>
2021-11-29 01:20:43 +0900

    Fix: unable to load magic-sdk

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,21 @@
 import createPersistentStore from '$utils/createPersistentStore'
+import type { Magic } from 'magic-sdk'
 
 let magic
 
-async function createMagic() {
+async function createMagic(): Promise<Magic> {
 	if (magic) return magic
 	else {
-		const Magic = (await import('magic-sdk')).default.Magic
+		const Magic = (await import('magic-sdk')).Magic
 		magic = new Magic(import.meta.env.VITE_MAGIC_PUBLIC_KEY as string)
 		return magic
 	}
 }
 
 export const userStore = createPersistentStore('user', null)
-// writable<null|{email: string, name: string|null}>(null)
 
 export async function login(email: string): Promise<{ email: string; name: null | string }> {
-	const magic = createMagic()
-	//@ts-ignore
+	const magic = await createMagic()
 	const didToken = await magic.auth.loginWithMagicLink({ email })
 
 	const res = await fetch('/api/login', {
