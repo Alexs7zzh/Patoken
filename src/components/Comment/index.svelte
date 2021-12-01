f4b79fc667263f57595eae46a3da00f61eb5b3d3
Alex Zhou <alex.zzh.s7@gmail.com>
2021-12-01 21:41:52 +0900

    Adjust positioning method

--- src/components/Comment/index.svelte
+++ src/components/Comment/index.svelte
@@ -12,7 +12,7 @@
 	let ref
 	let height = browser ? window.innerHeight : 500
 	let mobile = browser ? window.innerWidth < 680 : false
-	let heights = [0, -height * 0.4, -height * 0.85]
+	let heights = [height - 59, height * 0.6, height * 0.15]
 	let y = spring(heights[0], {
 		stiffness: 0.2,
 		damping: 0.75
@@ -29,15 +29,18 @@
 	function dragEnd(e) {
 		document.body.classList.remove('noscroll')
 		const oy = e.detail.offsetY
-		if (oy >= (heights[0] + heights[1]) / 2) y.set(heights[0])
+		if (oy >= (heights[0] + heights[1]) / 2) {
+			y.set(heights[0])
+			ref.classList.add('bottom')
+		} else ref.classList.remove('bottom')
 		if (oy < (heights[0] + heights[1]) / 2 && oy >= (heights[1] + heights[2]) / 2) y.set(heights[1])
 		if (oy < (heights[1] + heights[2]) / 2) y.set(heights[2])
 	}
 
 	function handleScroll() {
-		if (mobile && height !== window.innerHeight) {
-			ref.style.top = window.innerHeight - 59 + 'px'
+		if (mobile && ref.classList.contains('bottom') && height !== window.innerHeight) {
 			height = window.innerHeight
+			y.set(height - 59, { hard: true })
 		}
 	}
 
@@ -56,10 +59,11 @@
 <div
 	id="comment"
 	bind:this={ref}
+	class="bottom"
 	use:draggable={{
 		axis: 'y',
-		position: { x: 0, y: $y },
-		defaultPosition: { x: 0, y: $y },
+		position: { x: 0, y: mobile ? $y : 0 },
+		defaultPosition: { x: 0, y: mobile ? $y : 0 },
 		handle: '.handle',
 		...(!mobile && { disabled: true })
 	}}
@@ -76,9 +80,9 @@
 <style lang="scss">
 	:global(body.noscroll) {
 		touch-action: none;
-    overflow: hidden;
+		overflow: hidden;
 		-webkit-overflow-scrolling: contain;
-    overscroll-behavior: contain;
+		overscroll-behavior: contain;
 	}
 
 	#comment {
@@ -90,7 +94,7 @@
 			height: calc(85vh + 64px);
 			width: 100%;
 			position: fixed;
-			top: calc(100% - 59px);
+			top: 0;
 			border-top-left-radius: 0.6em;
 			border-top-right-radius: 0.6em;
 			background-color: var(--color-secondary-bg);
