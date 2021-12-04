80679ea1cf00ba9e9f22a46b951e153f7d472673
Alex Zhou <alex.zzh.s7@gmail.com>
2021-12-04 17:46:44 +0900

    Replace popperjs with floating-ui

--- src/components/Comment/Tooltip.svelte
+++ src/components/Comment/Tooltip.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
-	import { createPopper } from '@popperjs/core/lib/popper-lite'
-	import flip from '@popperjs/core/lib/modifiers/flip'
-	import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow'
-	import offset from '@popperjs/core/lib/modifiers/offset'
+	import { computePosition, flip, shift, offset } from '@floating-ui/dom'
 	import { onMount } from 'svelte'
 	import { currentComment, rangeToCurrentComment } from '$lib/comment'
 
-	let tooltip,
-		popper = null // reference to element
-	let range = null,
-		show = false
+	let tooltip
+	let range = null
+	let mobile = false
 
 	const virtualElement = {
 		getBoundingClientRect: () => range.getBoundingClientRect()
 	}
 
-	const destroyPopper = () => {
-		show = false
-		if (popper !== null) popper.destroy()
-		popper = null
+	const update = () => {
+		computePosition(virtualElement, tooltip, {
+			placement: mobile ? 'bottom' : 'top',
+			middleware: [flip(), shift({ padding: 12 }), offset(12)]
+		}).then(({ x, y }) => {
+			tooltip.style.transform = `translate3d(${Math.round(x)}px, ${Math.round(y)}px, 0)`
+		})
+	}
+
+	const resizeObserver = new ResizeObserver(update)
+
+	function showTooltip() {
+		tooltip.style.display = 'block'
+		update()
+		resizeObserver.observe(document.body)
+	}
+
+	function hideTooltip() {
+		tooltip.style.display = ''
+		resizeObserver.unobserve(document.body)
 	}
 
 	const handler = () => {
@@ -32,36 +44,20 @@
 				tmpRange.commonAncestorContainer.nodeName !== 'ARTICLE' &&
 				tmpRange.commonAncestorContainer.nodeName !== 'P')
 		) {
-			setTimeout(destroyPopper, 200)
+			setTimeout(hideTooltip, 200)
 		} else {
 			range = tmpRange
-			show = true
-			if (popper === null) {
-				popper = createPopper(virtualElement, tooltip, {
-					placement: window.matchMedia('(max-width: 680px)').matches ? 'bottom' : 'top',
-					modifiers: [
-						preventOverflow,
-						{
-							...flip,
-							options: {
-								padding: 60
-							}
-						},
-						{
-							...offset,
-							options: { offset: [0, 12] }
-						}
-					]
-				})
-			}
-			console.log(range.getBoundingClientRect())
-			popper.update()
+			showTooltip()
 		}
 	}
 
 	onMount(() => {
+		mobile = window.innerWidth < 680
 		document.addEventListener('selectionchange', handler)
-		return () => document.removeEventListener('selectionchange', handler)
+		return () => {
+			document.removeEventListener('selectionchange', handler)
+			resizeObserver.disconnect()
+		}
 	})
 
 	const addComment = () => {
@@ -70,28 +66,27 @@
 			currentComment.set(comment)
 			document.getSelection().removeAllRanges()
 		}
-		destroyPopper()
+		hideTooltip()
 	}
 </script>
 
-<div id="tooltip" role="tooltip" bind:this={tooltip} class:hidden={!show} on:click={addComment}>Comment</div>
+<div id="tooltip" role="tooltip" bind:this={tooltip} on:click={addComment}>Comment</div>
 
 <style lang="scss">
 	#tooltip {
+		display: none;
+		position: absolute;
+		top: 0;
+		left: 0;
 		background-color: var(--color-blue);
 		color: var(--color-black);
-		padding: 0.2em 0.3em;
+		padding: 0.1em 0.4em;
 		border-radius: 0.5em;
 		border: none;
 		font-size: 0.9em;
 		cursor: pointer;
 		transition: background-color 0.4s ease-out;
 		user-select: none;
-
-		&.hidden {
-			visibility: hidden;
-			pointer-events: none;
-		}
 
 		&:hover {
 			background-color: var(--color-melon);
