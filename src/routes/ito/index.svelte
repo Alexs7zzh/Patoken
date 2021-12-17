444838d7db6046588e75fb2e64142fd9c2ceab64
Alex Zhou <alex.zzh.s7@gmail.com>
2021-12-17 14:17:44 +0900

    Add ito

---
+++ src/routes/ito/index.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import Toggle from '$components/Header/Toggle.svelte'
+
+	let numRef, infoRef
+
+	const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+	const downHandler = () => {
+		infoRef.style.display = 'none'
+		numRef.textContent = String(random(1, 100))
+		numRef.style.opacity = 1
+	}
+
+	const upHandler = () => {
+		numRef.style.opacity = 0
+		setTimeout(() => {
+			infoRef.style.display = null
+		}, 300)
+	}
+</script>
+
+<svelte:head>
+	<title>Ito</title>
+</svelte:head>
+<svelte:window on:mousedown={downHandler} on:mouseup={upHandler} />
+<div class="container">
+	<h1>Ito</h1>
+	<p id="info" bind:this={infoRef}>Touch me</p>
+	<p id="num" bind:this={numRef}>50</p>
+	<Toggle style="top: calc(var(--spacing) + 0.5em); right: var(--spacing)" />
+</div>
+
+<style lang="scss">
+	:global(#svelte) {
+		display: block !important;
+		padding-bottom: 0 !important;
+	}
+
+	.container {
+		position: relative;
+
+		width: 100vw;
+		max-width: 600px;
+		height: 100vh;
+
+		margin-left: auto;
+		margin-right: auto;
+		padding: var(--spacing);
+	}
+
+	h1 {
+		margin-bottom: 0;
+		font-size: 2em;
+		line-height: 1;
+		user-select: none;
+	}
+
+	p {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		user-select: none;
+		font-weight: bold;
+	}
+
+	@keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		50% {
+			opacity: 0.7;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+
+	#info {
+		font-size: 5vw;
+		animation: fadeIn 2s infinite;
+	}
+
+	#num {
+		font-size: 35vw;
+		font-variant-numeric: tabular-nums;
+		opacity: 0;
+		transition: opacity 0.3s ease-out;
+	}
+</style>
