d171652f1330d4dc24254384e5a7d7c88f1f846f
Alex Zhou <alex.zzh.s7@gmail.com>
2022-01-27 16:39:40 +0900

    Bump dependencies

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,5 @@
 import { minify } from 'html-minifier'
 import { prerendering } from '$app/env'
-// import type { Handle } from '@sveltejs/kit'
 
 const options = {
 	removeAttributeQuotes: true,
@@ -14,16 +13,14 @@ const options = {
 	minifyJS: true
 }
 
-export const handle = async ({ request /* event */, resolve }) => {
-	const response = await resolve(request /* event */)
+export const handle = async ({ event, resolve }) => {
+	const response = await resolve(event)
 
-	if (prerendering && response.headers['content-type'] === 'text/html') response.body = minify(response.body, options)
-
-	// if (prerendering && response.headers.get('content-type') === 'text/html')
-	// 	return new Response(minify(await response.text(), options), {
-	//    	status: response.status,
-	//    	headers: response.headers
-	//    })
+	if (prerendering && response.headers.get('content-type') === 'text/html')
+		return new Response(minify(await response.text(), options), {
+			status: response.status,
+			headers: response.headers
+		})
 
 	return response
 }
